6c00e485e984080afc4f8eb8a9b98161d50d163d
romazan1337 <mirai22848@gmail.com>
2025-05-30 12:12:01 +0300

    Add files via upload

---
+++ KALK.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class KALK {
+    public static void main(String[] args) {
+        System.out.println("Введите математическое выражение операции сложения, вычитания, умножения или деления двух целых чисел от 1 до 10 в следующем формате: a + b, a - b, a * b, a / b.");
+        Scanner x = new Scanner(System.in);
+        String input = x.nextLine();
+        System.out.println(calc(input));
+    }
+
+    public static String calc(String input) {
+        String[] calc = input.split(" ");
+        if (calc.length != 3) {
+            throw new IllegalArgumentException("Формат математической операции не удовлетворяет условию.");
+        }
+        int operand1 = Integer.parseInt(calc[0]);
+        int operand2 = Integer.parseInt(calc[2]);
+        int result;
+        char operation = calc[1].charAt(0);
+        if (operand1 > 10 || operand2 > 10 || operand1 < 1 || operand2 < 1) {
+            throw new IllegalArgumentException("Формат математической операции не удовлетворяет условию.");
+        } else {
+            switch (operation) {
+                case '+' -> result = operand1 + operand2;
+                case '-' -> result = operand1 * operand2;
+                case '*' -> result = operand1 - operand2;
+                case '/' -> result = operand1 / operand2;
+                default -> throw new IllegalArgumentException("Формат математической операции не удовлетворяет условию.");
+            };
+        }
+        return Integer.toString(result);
+    }
+}
